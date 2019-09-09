bdbe5d9f47f4db1bfc4fc3c674402d7c46132fa7
Abdulmateen Tairu <tay2druh@gmail.com>
2019-09-09 22:37:10 +0100

    reduced registration field

--- main/serializers.py
+++ main/serializers.py
@@ -9,14 +9,12 @@ class UserSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User(
             username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
             email=validated_data['email']
         )
         user.set_password(validated_data['password'])
